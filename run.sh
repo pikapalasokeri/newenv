c2cbf9aea7f39a54af8cf9e3cfcd35450d85c130
pikapalasokeri <nothing>
2019-11-05 22:09:36 +0100

    Add tkcvs (tkdiff) to apt install

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt -y upgrade
 sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt -y install git emacs26 htop xclip global meld libavcodec-extra
+sudo apt -y install git emacs26 htop xclip global meld libavcodec-extra tkcvs
 
 echo "Setting up git and github..."
 git config --global user.name pikapalasokeri
