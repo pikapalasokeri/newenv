bfc42835e2c7a7230343f8172bb82883ac134c36
pikapalasokeri <nothing>
2019-11-10 22:44:55 +0100

    Add matplotlib

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt -y upgrade
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt -y install git emacs26 htop xclip global meld libavcodec-extra tkcvs python3-pip
-pip3 install pandas
+pip3 install pandas matplotlib
 
 echo "Setting up git and github..."
 git config --global user.name pikapalasokeri
