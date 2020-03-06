7de99908212af143a7c80a2161b0e5ec1ce0e66b
pikapalasokeri <noemail>
2020-03-06 21:53:36 +0100

    Install influxdb to server

--- setup_server.sh
+++ setup_server.sh
@@ -3,3 +3,13 @@ sudo apt update
 sudo apt upgrade
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt -y install git emacs26 htop python3-pip
+
+# Install influxdb
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt install influxdb
+sudo systemctl unmask influxdb.service
+sudo systemctl start influxdb
+
+sudo apt install influxdb-client
