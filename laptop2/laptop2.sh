a333756ba2058c14480e5a57003655be9a5e59f1
pikapalasokeri <noemail>
2020-12-01 15:36:15 +0100

    Add some info on new laptop graphics and power management

---
+++ laptop2/laptop2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+echo "Install nvidia drivers: ubuntu-drivers install"
+
+echo "Fix nvidia screen tearing with grub: https://askubuntu.com/questions/1194557/cant-force-prime-synchronization-screen-tearing/1266161#1266161"
+echo "Do some tlp stuff: https://askubuntu.com/a/1134726"
+echo "Copy tlp.conf to /etc/tlp.conf"
